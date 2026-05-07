39334b8d710934cf3d0327faba150415adcd34d4
TPS Bank <danilo.soru@tempocasa.com>
2026-05-07 14:23:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Gestione del saldo
+let currentBalance = 0.00;
+const balanceDisplay = document.getElementById('balance');
+const btnBoost = document.getElementById('btn-boost');
+const btnDeduct = document.getElementById('btn-deduct');
 
+function updateDisplay() {
+    balanceDisplay.innerText = `€ ${currentBalance.toFixed(2)}`;
+}
+
+btnBoost.addEventListener('click', () => {
+    currentBalance += 10.00;
+    updateDisplay();
+});
+
+btnDeduct.addEventListener('click', () => {
+    if(currentBalance >= 10) {
+        currentBalance -= 10.00;
+        updateDisplay();
+    }
+});
